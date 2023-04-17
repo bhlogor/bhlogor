253ad7770f814ad67400a4ad1e37f8fbe731b3bf
bhlogor <123261095+bhlogor@users.noreply.github.com>
2023-04-17 17:26:36 +0700

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,13 +3,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <>
         <header className="flex flex-wrap items-center justify-between bg-white px-8 py-5 shadow-md shadow-slate-900/5 transition duration-500">
-          Post Example
+          PostWeb
         </header>
         <main className='max-w-screen-sm px-5 py-8 mx-auto'>
           {children}
         </main>
         <footer className="items-center justify-between bg-white px-8 py-5 border-t border-slate-900/5 transition duration-500">
-          2023 PostExample
+          2023 #PostWeb
         </footer>
       </>
     )
